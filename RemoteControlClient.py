5de9eed61967f0f7ad091a46255ba76855c983bd
Oliver <oliverodd@gmail.com>
2017-10-17 15:00:20 +0200

    first push

--- RemoteControlClient.py
+++ RemoteControlClient.py
@@ -12,13 +12,13 @@ class KeyEvent:
 
 class KeyboardListener:
     def __init__(self):
-        self._event = False
+        self._event = list()
         self._keyStatus = dict()
 
     def getEvent(self):
-        e = self._event
-        self._event = False
-        return e
+        if(self._event):
+            return self._event.pop(0)
+        return False
 
     def on_press(self, key):
         key=key.char
@@ -26,7 +26,7 @@ class KeyboardListener:
         if key not in self._keyStatus:
             self._keyStatus[key] = False
         if not self._keyStatus[key]:
-            self._event = KeyEvent(key, KeyEvent.Event.Pressed)
+            self._event.append(KeyEvent(key, KeyEvent.Event.Pressed))
             self._keyStatus[key] = True
 
 
@@ -35,7 +35,7 @@ class KeyboardListener:
         if key not in self._keyStatus:
             self._keyStatus[key] = True
         if self._keyStatus[key]:
-            self._event = KeyEvent(key, KeyEvent.Event.Released)
+            self._event.append(KeyEvent(key, KeyEvent.Event.Released))
             self._keyStatus[key] = False
 
 
@@ -45,27 +45,41 @@ listener = keyboard.Listener(on_press=k.on_press, on_release=k.on_release)
 
 running = False
 
+host = 'localhost'
+
 #Skapar socket med Address Family 'Internet' och är en streaming socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
 try:
     #Connectar tiill servern och startar keyboard listener
-    clientSocket.connect(('localhost', 9000))
+    clientSocket.connect((host, 9000))
     listener.start()
     running = True
 except TimeoutError:
     print("Could not connect to server")
     exit(1)
 
-print("Connected to: localhost:9000")
+print("Connected to:", host, ":9000")
+#print("Connected to: localhost:9000")
 while running:
     if not listener.running:
         listener = keyboard.Listener(on_press=k.on_press, on_release=k.on_release)
         listener.start()
     event = k.getEvent()
-    if event is not False:
+    if event:
         if event.type is KeyEvent.Event.Pressed:
             print('key pressed: ' + str(event.key))
+            #data = event.key + ':1'
+            #clientSocket.sendall(data.encode('utf-8'))
             clientSocket.sendall(event.key.encode('utf-8'))
         elif event.type is KeyEvent.Event.Released:
             print('key released: ' + str(event.key))
+            #data = event.key + ':0'
+            #clientSocket.sendall(data.encode('utf-8'))
             clientSocket.sendall(event.key.encode('utf-8'))
+
+def main(arg):
+    print(arg)
+    global host
+    host = arg
+
+main(sys.argv[1])
--- RemoteControlServer.py
+++ RemoteControlServer.py
@@ -1,26 +1,57 @@
 from socket import *
 
+host = 'localhost'
+
 #skapa en streamingsocket med adresstyp internet (TCP) som väntar på klienten
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('localhost', 9000))
+#print('1')
+serverSocket.bind((host, 9000))
+#print('2')
 serverSocket.listen(1)
 
+#print('väntar på client')
 #vänta på att klienten ska connecta
 (clientSocket, address) = serverSocket.accept()
+#print('accepted')
 serverSocket.close()
 
 print("Client: " + str(address[0]) + ":" + str(address[1]) + " connected")
 running = True
 
 forward = False
+backward = False
+right = False
+left = False
+
 
 while running:
     #ta emot 8 byte data, konvertera till utf-8 och printa
+    #data = clientSocket.recv(8)
+    #num = int(data)
+    #data2 = clientSocket.recv(8).decode('ascii')
     data = clientSocket.recv(8).decode('utf-8')
     if data is 'w':
         forward = not forward
         print('Forward toggle: ' + str(forward))
-
-    print(data)
+    elif data is 's':
+        backward = not backward
+        print('Backward toggle: ' + str(backward))
+    elif data is 'a':
+        left = not left
+        print('Left toggle: ' + str(left))
+    elif data is 'd':
+        right = not right
+        print('Right toggle: ' + str(right))
+    else:
+        print("data: ",data)
+    #print("data2: ",data2)
+    #print("num: ",num)
 
 clientSocket.close()
+
+def main(arg):
+    print(arg)
+    global host
+    host = arg
+
+main(sys.argv[1])
